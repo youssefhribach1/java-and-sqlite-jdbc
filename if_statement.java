e4ce4f8949e2cd1b21ea9e9b13f1888f14125d45
youssefhribach1@gmail.com <java0109>
2019-04-15 22:23:56 +0200

    15/04/2019

---
+++ if_statement.java
@@ -0,0 +1,21 @@
+package projet1;
+import java.util.Scanner;
+public class if_statement {
+  static Scanner reader=new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age;
+		String fullname;
+		System.out.println("Enter your Full name please:");
+		fullname=reader.nextLine();
+		System.out.println("Enter your age:");
+		age=reader.nextInt();
+		if(age>=18) {
+			System.out.println("Welcome mr "+fullname);
+		}
+		else {
+		     System.out.println("u'r not accepted mr: "+fullname);
+		}
+	}
+
+}
---
+++ increment_and_decrement.java
@@ -0,0 +1,20 @@
+package projet1;
+import java.util.Scanner;
+public class increment_and_decrement {
+
+	static Scanner reader=new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		
+		System.out.println("Enter a number! :");
+		  int n1=reader.nextInt();
+			//n1=n1+1;
+		    //n1++;
+		    //n1--;
+		   n1=n1-1;
+		System.out.println("Result :"+n1);
+		  
+	}
+
+}
