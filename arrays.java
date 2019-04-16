f64d3d0f072525cc9ffcfa1f891a11c36d2f3850
youssefhribach1@gmail.com <java0109>
2019-04-16 22:44:58 +0200

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,29 @@
+package projet1;
+
+public class arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+     String[]names=new String[5];
+     
+     names[0]="lina";
+     names[1]="youssef";
+     names[2]="adil";
+     names[3]="anwar";
+     names[4]="hassan";
+     
+ System.out.println(names[2]);
+ System.out.println(names[1]);
+ System.out.println(names[0]);
+ System.out.println(names[3]);
+ System.out.println(names[4]);
+ 
+ 
+ // String[]numbers={"1","2","3","155","545","222"};
+
+     
+
+	}
+
+}
